7b7a02e660e1d0cf65bc789699df232261e2ada6
MokshChoudhary <moksh.choudhary.2@google.com>
2022-11-06 03:48:17 +0530

    HomeScreen created, HomeView created, CardView for HomeView

---
+++ lib/constant/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class AppColor {
+  //light theme
+  static const Color primaryColor = Color.fromARGB(255, 226, 141, 5);
+  static const Color secondaryColor = Color.fromARGB(255, 59, 59, 59);
+  static const Color textColor = Colors.black;
+  static const Color secondaryTextColor = Colors.white;
+}
--- lib/contants/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-// ignore: camel_case_types
-class AppColor {
-  //light theme
-  static Color primaryColor = Colors.yellow.shade400;
-  static Color secondryColor = Colors.grey.shade400;
-  static Color textColor = Colors.black87;
-}
